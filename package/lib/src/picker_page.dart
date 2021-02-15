c1da25af4fe8cb4202e6f59d216d77ef8eb3bb64
Muhammad Wahyu Pamengas <wahyu.darkflame@gmail.com>
2021-02-15 09:29:15 +0800

    test

--- package/lib/src/picker_page.dart
+++ package/lib/src/picker_page.dart
@@ -187,7 +187,7 @@ class _FilesystemPickerState extends State<FilesystemPicker> {
   }
 
   void _changeDirectory(Directory value) {
-    if (directory != value) {
+    if (directory.absolute.path != value.absolute.path) {
       setState(() {
         _setDirectory(value);
       });
